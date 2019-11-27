e6177873cfcc3792e9d3d7b9bf02164816f220a6
Bilal Sevinc <bilalresmi@gmail.com>
2019-11-27 23:32:59 +0300

    deploying

---
+++ precache-manifest.709aed36b94e9c7552cb26e2cbe46f19.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5885ee830ffbb9abb8b618f39b89cc22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "042af29522fd4184e7cb",
+    "url": "/static/css/main.a689640d.chunk.css"
+  },
+  {
+    "revision": "07df643a449e4edc9427",
+    "url": "/static/js/2.524bc227.chunk.js"
+  },
+  {
+    "revision": "042af29522fd4184e7cb",
+    "url": "/static/js/main.bf76b885.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  },
+  {
+    "revision": "b2427ac7d5cb8dd9e13102f021e8fd57",
+    "url": "/static/media/agency.b2427ac7.png"
+  },
+  {
+    "revision": "de60b33348cb4aa3ad6b10f280e8faf3",
+    "url": "/static/media/article.de60b333.png"
+  },
+  {
+    "revision": "d9f8f8b3aee7ba759e9c87f22ddca1fb",
+    "url": "/static/media/coke.d9f8f8b3.png"
+  },
+  {
+    "revision": "5498dd15b1d8eb1c4548fc164c936913",
+    "url": "/static/media/moon.5498dd15.svg"
+  },
+  {
+    "revision": "6bdf8c3110c23e2cb104f25fa48625bb",
+    "url": "/static/media/recipe.6bdf8c31.png"
+  },
+  {
+    "revision": "86f6a57f320b6523428e06e6013ecc39",
+    "url": "/static/media/rental.86f6a57f.png"
+  },
+  {
+    "revision": "771019f4c6867f2adaa5abeb1448c70c",
+    "url": "/static/media/sun.771019f4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cc888dcc7b1cd1237e670f6382530c34.js"
+  "/precache-manifest.709aed36b94e9c7552cb26e2cbe46f19.js"
 );
 
 self.addEventListener('message', (event) => {
