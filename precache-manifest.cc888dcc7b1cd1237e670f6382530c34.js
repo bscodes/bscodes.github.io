370167c43d69eda8f2238e563d3ac7d5631a5cda
Bilal Sevinc <bilalresmi@gmail.com>
2019-11-27 23:06:44 +0300

    first build

---
+++ precache-manifest.cc888dcc7b1cd1237e670f6382530c34.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a0c076cf4adecaae8cc77a72905e46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59e963b56e6864e5773e",
+    "url": "/static/css/main.86774b4c.chunk.css"
+  },
+  {
+    "revision": "07df643a449e4edc9427",
+    "url": "/static/js/2.524bc227.chunk.js"
+  },
+  {
+    "revision": "59e963b56e6864e5773e",
+    "url": "/static/js/main.bf76b885.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  },
+  {
+    "revision": "b2427ac7d5cb8dd9e13102f021e8fd57",
+    "url": "/static/media/agency.b2427ac7.png"
+  },
+  {
+    "revision": "de60b33348cb4aa3ad6b10f280e8faf3",
+    "url": "/static/media/article.de60b333.png"
+  },
+  {
+    "revision": "d9f8f8b3aee7ba759e9c87f22ddca1fb",
+    "url": "/static/media/coke.d9f8f8b3.png"
+  },
+  {
+    "revision": "5498dd15b1d8eb1c4548fc164c936913",
+    "url": "/static/media/moon.5498dd15.svg"
+  },
+  {
+    "revision": "6bdf8c3110c23e2cb104f25fa48625bb",
+    "url": "/static/media/recipe.6bdf8c31.png"
+  },
+  {
+    "revision": "86f6a57f320b6523428e06e6013ecc39",
+    "url": "/static/media/rental.86f6a57f.png"
+  },
+  {
+    "revision": "771019f4c6867f2adaa5abeb1448c70c",
+    "url": "/static/media/sun.771019f4.svg"
+  }
+]);
