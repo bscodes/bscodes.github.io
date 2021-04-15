4435f9146e9953c83ad498c9aafaedaf11791cb7
Bilal Sevinc <bilalresmi@gmail.com>
2021-04-15 15:31:16 +0300

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Hey from '../components/Hey';
-import What from '../components/What';
 import About from '../components/About';
 import Me from '../components/Me';
 
 const Home = () => {
     return(
         <>
-          <Hey/>  
-          <What/>  
+          <Hey/> 
           <About/>  
           <Me/>  
         </>
