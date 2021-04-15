230ca6e3d084be2f23003f7e4aaf836fefa853c8
Bilal Sevinc <bilalresmi@gmail.com>
2021-04-15 15:33:01 +0300

    Updates

---
+++ precache-manifest.0d3619eac59f1fd7e8edceb0660ab38c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e0d7d5a65bea61783da00c25e9cf41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef788a6cab10d6f2b0ea",
+    "url": "/static/css/main.c997afcc.chunk.css"
+  },
+  {
+    "revision": "522f42e897af53eddfc0",
+    "url": "/static/js/2.80e07787.chunk.js"
+  },
+  {
+    "revision": "ef788a6cab10d6f2b0ea",
+    "url": "/static/js/main.b37707bd.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  },
+  {
+    "revision": "5498dd15b1d8eb1c4548fc164c936913",
+    "url": "/static/media/moon.5498dd15.svg"
+  },
+  {
+    "revision": "771019f4c6867f2adaa5abeb1448c70c",
+    "url": "/static/media/sun.771019f4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1593586b7cead65898c8296223ab14a9.js"
+  "/precache-manifest.0d3619eac59f1fd7e8edceb0660ab38c.js"
 );
 
 self.addEventListener('message', (event) => {
